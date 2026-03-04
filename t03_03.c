0c18de2a3302f6b95fcf1ca38f6f1bf7e3e14e95
si25itdel <simatupangrafael100@gmail.com>
2026-03-04 14:55:37 +0700

    12S25006
    DONE

--- t03_03.c
+++ t03_03.c
@@ -1,34 +1,29 @@
-#include <stdio.h> 
-#include <limits.h>
-#include <float.h> 
+#include <stdio.h>
+#include <limits.h>  
+#include <float.h>  
 
 int main() {
     int n;
     int num; 
-    int prev_num;
+    int prev_num; 
+
     int min_val = INT_MAX;
     int max_val = INT_MIN;
-    float min_consecutive_avg = FLT_MAX;
 
-    int third_element = 0;
-    int fourth_element = 0;
-    float average_third_fourth = 0.0;
+    float min_consecutive_avg = FLT_MAX; 
+    float max_consecutive_avg = -FLT_MAX; 
 
-    int i;
+    int i;          
     scanf("%d", &n);
-    if (n > 0) {
-        scanf("%d", &prev_num);
-        min_val = prev_num;
-        max_val = prev_num;
+    if (n <= 0) {
+        return 0;
     }
+
+    scanf("%d", &prev_num);
+    min_val = prev_num;
+    max_val = prev_num;
     for (i = 1; i < n; i++) {
         scanf("%d", &num);
-        if (i == 2) { 
-            third_element = num;
-        } else if (i == 3) {
-            fourth_element = num;
-        }
-
         if (num < min_val) {
             min_val = num;
         }
@@ -41,18 +36,17 @@ int main() {
             min_consecutive_avg = current_avg;
         }
 
+        if (current_avg > max_consecutive_avg) {
+            max_consecutive_avg = current_avg;
+        }
+
         prev_num = num;
-    }
-    if (n >= 4) {
-        average_third_fourth = (float)(third_element + fourth_element) / 2.0;
-    } else {
-        average_third_fourth = 0.0;
     }
 
     printf("%d\n", min_val);
     printf("%d\n", max_val);
     printf("%.2f\n", min_consecutive_avg);
-    printf("%.2f\n", average_third_fourth);
+    printf("%.2f\n", max_consecutive_avg);
 
-    return 0;
+    return 0; 
 }
